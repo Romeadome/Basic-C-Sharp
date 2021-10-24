ab78a0ff54884172b0dd052270c41fddf4678831
Roman Love <66181398+Romeadome@users.noreply.github.com>
2021-10-24 18:49:29 -0400

    small projects for practice

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////evaluates to false
+            Console.WriteLine(true && false);
+            //evaluates to true
+            Console.WriteLine(true && true);
+            //evaluates to false
+            Console.WriteLine(false && false);
+            //evalutes to true
+            Console.WriteLine(true || false);
+            //evaluates to true
+            Console.WriteLine(true || true);
+            //evaluates to false
+            Console.WriteLine(false || false);
+            //evaluates true
+            Console.WriteLine(false || true);
+            //evaluates true
+            Console.WriteLine(true == true);
+            //evaluates false
+            Console.WriteLine(true == false);
+            //false
+            Console.WriteLine(false == false);
+            //evaluates false
+            Console.WriteLine(true != true);
+            //evaluates true
+            Console.WriteLine(true != false);
+            //evaluates false
+            Console.WriteLine(false != false);
+            //evaluates false
+            Console.WriteLine(true ^ true);
+            //evaluates true
+            Console.WriteLine(true ^ false);
+            //evaluatesfalse
+            Console.WriteLine(false ^ false);
+
+            /*The XOR (^) operator will evaluates to true if one is true but not both */
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Small Array Project/Small Array Project/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Small_Array_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string[] stringArray = { "Roman", "Jesse", "John", "Sally", "Elizabeth", "Beth" };
+               Console.WriteLine("Select an index value between 0 and 5 of the string array: ");
+               int stringIndex = Convert.ToInt32(Console.ReadLine());
+            while (stringIndex > 5 || stringIndex < 0 )
+            {
+                Console.WriteLine("Error! Value doesn't exist!");
+                Console.WriteLine("Select an index value between 0 and 5 of the string array: ");
+                stringIndex = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] intArray = { 40, 35, 5060, 110, 334, 6000 };
+            Console.WriteLine("Select an index value between 0 and 5 of the int array:");
+            int intIndex = Convert.ToInt32(Console.ReadLine());
+            while (intIndex > 5 || intIndex < 0)
+            {
+                Console.WriteLine("Error! Value doesn't exist!");
+                Console.WriteLine("Select an index value between 0 and 5 of the int array: ");
+                intIndex = Convert.ToInt32(Console.ReadLine());
+            }
+
+            List<string> stringList = new List<string>();
+            stringList.Add("Roman");
+            stringList.Add("Hello");
+            stringList.Add("Today is the day");
+            stringList.Add("Goodbye");
+            Console.WriteLine("Select an index value between 0 and 3 of the string list:");
+            int listIndex = Convert.ToInt32(Console.ReadLine());
+            while (listIndex > 3 || listIndex < 0)
+            {
+                Console.WriteLine("Error! Value doesn't exist!");
+                Console.WriteLine("Select an index value between 0 and 3 of the string list: ");
+                intIndex = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The value inputted for the string array was: " + stringIndex + " This resulted a output string of: "
+                + stringArray[stringIndex] + "\n");
+            Console.WriteLine("The value inputted for the int array was: " + intIndex + " This resulted a output string of: "
+                + intArray[intIndex] + "\n");
+            Console.WriteLine("The value inputted for the string array was: " + listIndex + " This resulted a output string of: "
+                + stringList[listIndex] + "\n");
+            Console.Read();
+        }
+    }
+}
