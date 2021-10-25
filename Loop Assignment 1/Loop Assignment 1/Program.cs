23192e9c0d375b49106e9fe9d312c1a8caba2b9d
Roman Love <66181398+Romeadome@users.noreply.github.com>
2021-10-25 18:05:15 -0400

    Practice with iterations and loops

---
+++ Loop Assignment 1/Loop Assignment 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] stringArray = {"One ", "Two ", "Three ", "Four ", "Five ", "Six "};
+
+        Console.WriteLine("Input a string of text");
+        string userInput = Console.ReadLine();
+
+        for (int i = 0; i < stringArray.Length; i++)
+        {
+            Console.WriteLine(stringArray[i] + userInput);
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Loop Assignment 3/Loop Assignment 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Add the following to your console app and perform these actions:
+1. A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
+2. Save and execute your code.
+3. A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
+4. Save and execute your code.
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("This loop iterates while variable i is less than 10. This is iteration: " + i);
+          
+        }  
+        for (int j = 0; j <= 10; j++)
+            {
+                Console.WriteLine("This loop iterates while variable j is less than i. This is iteration: " + j);
+            }
+        Console.ReadLine();
+    }
+}
