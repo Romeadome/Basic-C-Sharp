8d2bf223f2ecb59d4745b181b0f84d82cc46af1b
Roman Love <66181398+Romeadome@users.noreply.github.com>
2021-10-29 16:36:30 -0400

    additional practice assignments

---
+++ Loop Assignment 5/Loop Assignment 5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Add the following to your console app and perform these actions:
+1. A list of strings that has at least two identical strings in the list. Ask the user to select text to search for in the list. 
+2. Create a loop that iterates through the loop and then displays the indices of the array that contain matching text on the screen.
+3. Add code to the loop that tells a user if they put in text that isn’t in the list.
+4. Save and execute your code.
+*/
+namespace Loop_Assignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string>() { "Cat", "Dog", "Bird", "Turtle","Rooster", "Hamster", "Ferret", "Bird", "Snake", "Spider", "Bird", "Rooster" };
+            Console.WriteLine("Search for type of animal:");
+            string userInput = Console.ReadLine();
+            int count = 0;
+
+            foreach (string animal in animals)
+            {
+                if (userInput.ToUpper() == animal.ToUpper())
+                {
+                    int index = 0;
+                    foreach (string animal2 in animals )
+                    //for (int k = 0; k < animals.Count; k++)
+                    {
+                         if (userInput.ToUpper() == animal2.ToUpper()) 
+                        {
+                            Console.WriteLine("Animal \"{0}\" was found at index: {1}", animal2, index);
+                        }
+                        index++;
+                    }
+                    break;
+                }
+                count++;
+                if (count == animals.Count)
+                {
+                    Console.WriteLine("Your animal does not exist in the list");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Loop Assignment 6/Loop Assignment 6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Add the following to your console app and perform these actions:
+1. Create a list of strings that has at least two identical strings in the list. 
+2. Create a foreach loop that evaluates each item in the list, and displays a
+message showing the string and whether or not it has already appeared in the list.
+3. Save and execute your code.
+
+ */
+namespace Loop_Assignment_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string>() { "Cat", "Dog", "Bird", "Turtle", "Hamster", "Ferret", "Bird", "Snake", "Spider", "Bird" };
+            Console.WriteLine("Search for type of animal:");
+            string userInput = Console.ReadLine();
+            int count = 0;
+
+            foreach (string animal in animals)
+            {
+                if (userInput.ToUpper() == animal.ToUpper())
+                {
+                    int index = 1;
+                    foreach (string animal2 in animals)
+                    //for (int k = 0; k < animals.Count; k++)
+                    {
+                        if (userInput.ToUpper() == animal2.ToUpper())
+                        {
+                            Console.WriteLine("Animal \"{0}\" was found. This appears {1} time(s) in the list.", animal2, index);
+                            index++;
+                        }
+                        
+                    }
+                    break;
+                }
+                count++;
+                if (count >= animals.Count)
+                {
+                    Console.WriteLine("Your animal does not exist in the list");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
