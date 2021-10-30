a07342734bef14ce5add25940c6ab5fe39603cb0
Roman Love <66181398+Romeadome@users.noreply.github.com>
2021-10-30 17:10:08 -0400

    review program of exception handling

---
+++ Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a second number: ");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dividing the two numbers.......");
+                int number3 = number1 / number2;
+                Console.WriteLine("{0} divided by {1} equals {2}", number1, number2, number3);
+                Console.ReadLine();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
