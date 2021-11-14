155f20e12ffbc93c299052d5dd835bcc1a26f8d9
Roman Love <66181398+Romeadome@users.noreply.github.com>
2021-11-14 18:59:40 -0500

    Method Overloading Example

---
+++ Overloaded Math Functions/Overloaded Math Functions/Math Method.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Math_Functions
+{
+    class Math_Method
+    {
+      
+        public int Math(int input)
+        {
+            int result = 3*input * input;
+            return result;
+        }
+        public int Math(decimal input)
+        {
+            int result = Decimal.ToInt32(input) + Decimal.ToInt32(input);
+            return result;
+        }
+        public int Math(string input)
+        {
+            int result = Int32.Parse(input) * Int32.Parse(input);
+            return result;
+        }
+
+
+    }
+}
