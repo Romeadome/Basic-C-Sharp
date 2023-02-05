063ce9e4f1d46e13b7dd0ced30d334c9d7586626
Roman Love <66181398+Romeadome@users.noreply.github.com>
2023-02-05 15:57:39 -0500

    Added Assignment details to #107

--- 107- Classes and Methods Assignment/Class And Methods Assignment/Class And Methods Assignment/Program.cs
+++ 107- Classes and Methods Assignment/Class And Methods Assignment/Class And Methods Assignment/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ ASSIGNMENT
+Perform these actions and create a console app that includes the following:
+1. Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. 
+The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
+
+2. In the Main() program, ask the user what number they want to do the math operations on.
+
+3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+*/
 namespace Class_And_Methods_Assignment
 {
     internal class Program
